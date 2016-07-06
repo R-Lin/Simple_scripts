7ffe636a38dab589c3ee033e3b2df0f00c110139
Lin-R <linkaixuan@yaochufa.com>
2016-07-06 18:31:38 +0800

    DNS_CHECK脚本更新

--- SHELL/dns_check.sh
+++ SHELL/dns_check.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 TIMEOUT=3
 DOMAIN='dns.test.ycf.com'
-
-RESULT=`nslookup ${DOMAIN} -timeout=${TIMEOUT}`
-RETURN_CODE=$?
-if [ ${RETURN_CODE} -eq 0 ]
-then
-	# awk 如果gsub替换成功,返回值为替换次数
-	# result不为0, 代表有预期的结果,解析正确
-	awk -vdomain=${DOMAIN} '{
-		result=gsub("111.111.111.111","")
-		print result==0?1:0
-	}' <<<${RESULT}
-	
-else :
-	echo 1
-	
-fi
+nslookup ${DOMAIN} -timeout=${TIMEOUT} &> /dev/null
+[ $? -eq 0 ] && echo 0 || echo 1
