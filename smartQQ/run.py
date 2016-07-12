30f0266629ea77ef52bff62368126dc31c4554b1
Lin-R <linkaixuan@yaochufa.com>
2016-07-12 16:35:49 +0800

    整合了扫码登录和cookies登录

--- smartQQ/run.py
+++ smartQQ/run.py
@@ -4,21 +4,25 @@ import sys
 import time
 import json
 import ini
+import os
+import cPickle
+import requests
 
 
-class SmartQQ():
+class SmartQQ:
     """
     A simple robot! For Fun!
     """
     def __init__(self):
         self.qtwebqq = None
+        self.cookie_file = "cookies.txt"
         self.clientid = 53999199
         self.psessionid = ''
         self.vfwebqq = None
         self.para_dic = {}
         self.url_request = ini.get_req()
         self.log = ini.log()
-        self.groupName ={}
+        self.groupName = {}
         self.groupMember = {}
         self.url_dic = {
             'qrcode': 'https://ssl.ptlogin2.qq.com/ptqrshow?appid={0}&e=0&l=L&s=8&d=72&v=4',
@@ -26,33 +30,44 @@ class SmartQQ():
             'groupNameList': 'http://s.web2.qq.com/api/get_group_name_list_mask2',
             'groupInfo': 'http://s.web2.qq.com/api/get_group_info_ext2?gcode={0}&vfwebqq={1}&t={2}',
             'pollMessage': 'http://d1.web2.qq.com/channel/poll2',
-            'para': "".join((
-                'https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq',
-                '&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&',
-                'f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001')),
-            'check_scan': ''.join((
-                'https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid={0[appid]}',
-                '&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=',
-                '2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-0&mibao_css={0[mibao_css]}',
-                '&t=undefined&g=1&js_type=0&js_ver={0[js_ver]}&login_sig={0[sign]}&pt_randsalt=0'))}
+            'para': (
+                'https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq'
+                '&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&'
+                'f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001'),
+            'check_scan': (
+                'https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid={0[appid]}'
+                '&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang='
+                '2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-0&mibao_css={0[mibao_css]}'
+                '&t=undefined&g=1&js_type=0&js_ver={0[js_ver]}&login_sig={0[sign]}&pt_randsalt=0')
+        }
 
-    def downQrcode(self):
+    def qrcode_login(self):
         """
-         Download the Qrcode png
+         Downing the QRcode, and scan it to login
         """
-        try:
-            url = self.url_dic['qrcode'].format(
-                self.para_dic['appid']
+        url = self.url_dic['qrcode'].format(self.para_dic['appid'])
+        with open('qrcode.png', 'wb') as f:
+            f.write(self.url_request.get(url, verify=True).content)
+            self.log.info('Qrcode file is qrcode.png ! Please scan qrcode immediatety')
+        url = self.url_dic['check_scan'].format(self.para_dic)
+        while 1:
+            result = eval(self.url_request.get(url, verify=True).text[6:-3])
+            self.log.info(result[4])  # return login result
+            if result[0] == '0':
+                redirect_url = result[2]
+                self.url_request.get(redirect_url)  # visit redirect_url to modify the session cookies
+                break
+            time.sleep(4)
+
+        # Save the cookies
+        with open(self.cookie_file, 'w') as filename:
+            cPickle.dump(
+                requests.utils.dict_from_cookiejar(self.url_request.cookies),
+                filename
             )
-            with open('qrcode.png', 'wb') as f:
-                f.write(self.url_request.get(url, verify=True).content)
-                self.log.info('Qrcode file is qrcode.png ! Please scan qrcode immediatety')
-        except Exception as messages:
-            self.log.error(messages)
-            self.log.error('Webbrowser open or down failed! Please retry')
-            sys.exit()
+            self.log.info("Cookies had saved")
 
-    def getPara(self):
+    def get_comm_para(self):
         """
         Return a dict that contains appid, sign, js_ver, mibao_cass
         """
@@ -62,20 +77,23 @@ class SmartQQ():
         self.para_dic['js_ver'] = re.findall(r'g_pt_version=encodeURIComponent\("(\d+)"\)', html.text)[0]
         self.para_dic['mibao_css'] = re.findall(r'g_mibao_css=encodeURIComponent\("(.+?)"\)', html.text)[0]
 
-    def checkLogin(self):
-        """
-        Loop to check the QRcode status
-        """
-        url = self.url_dic['check_scan'].format(self.para_dic)
-        while 1:
-            result = eval(self.url_request.get(url, verify=True).text[6:-3])
-            self.log.info(result[4])                # return login result
-            if result[0] == '0':
-                redirect_url = result[2]
-                self.url_request.get(redirect_url)  # visit redirect_url to modify the session cookies
-                break
-            time.sleep(4)
-
+    def login(self):
+        if os.path.exists(self.cookie_file):
+            cookies_file_mtime = os.stat(self.cookie_file).st_mtime
+            during_time = time.time() - cookies_file_mtime
+            if during_time <= 7200:
+                with open(self.cookie_file) as f:
+                    cookies = requests.utils.cookiejar_from_dict(
+                        cPickle.load(f)
+                    )
+                    self.url_request.cookies.update(cookies)
+                    self.log.info("Cookies had loaded")
+            else:
+                self.log.warn('Cookies_file time out')
+                self.qrcode_login()
+        else:
+            self.log.info("Cookies file not found! Please scan the QRcode")
+            self.qrcode_login()
         self.qtwebqq = self.url_request.cookies['ptwebqq']
         r_data = {
             'r': '{{"ptwebqq":"{0}","clientid":{1},"psessionid":"{2}","status":"online"}}'.format(
@@ -95,15 +113,15 @@ class SmartQQ():
                     )
         result2 = json.loads(self.url_request.get(vfwebqq_url).text)
         self.vfwebqq = result2['result']['vfwebqq']
+        activate_url = self.url_dic['get_online_buddies2'].format(self.vfwebqq, self.clientid, self.psessionid)
         activate = json.loads(
-            self.url_request.get(self.url_dic['get_online_buddies2']).text
+            self.url_request.get(activate_url).text
             )
-
-        if activate['retcode']:
+        if not activate['retcode']:
             self.log.info('Activate suessfully!')
         else:
             self.log.error(
-                'Activate failed! Retcode:', activate.get('retcode', "wrong")
+                'Activate failed! Retcode: %s' % activate['retcode']
             )
 
     def poll(self):
@@ -111,15 +129,16 @@ class SmartQQ():
         Poll the messages
         """
         if not self.vfwebqq or not self.psessionid:
-            self.log("Please login")
+            self.log.info("Please login")
             self.login()
         else:
-            data = {'r':json.dumps(
-                {"ptwebqq": self.qtwebqq,
+            data = {'r':json.dumps({
+                "ptwebqq": self.qtwebqq,
                  "clientid": self.clientid,
                  'psessionid': self.psessionid,
                  "key": ""
-                 })}
+                 }
+            )}
             # print self.url_request.post('https://httpbin.org/post', data=data).text
             # sys.exit()
             while 1:
@@ -136,7 +155,7 @@ class SmartQQ():
                         elif messages['poll_type'] == 'group_message':
                             if from_uin not in self.groupMember:
                                 self.log.info('GroupId : %s is not in dict GroupName' % from_uin)
-                                self.groupInfo(from_uin)
+                                self.get_group_info(from_uin)
                             send_uid = str(messages['value']['send_uin'])
                             print self.groupName[from_uin]['name'],
                             print self.groupMember[from_uin][send_uid] + ":" + words
@@ -146,25 +165,25 @@ class SmartQQ():
                         self.log.error('KeyError: 131 lines')
                         self.log.error(m)
                         print 133, self.groupName
-                        # print 134, groupid, send_uid, self.groupMember
                         print mess
+                    else:
+                        self.log.info("Messages time out ! ")
+
                 except TypeError as e :
                     self.log.error(e)
-                    self.log.error('TypeError: 154 lines')
+                    self.log.error('TypeError: 176 lines')
                     self.log.error(reponse)
 
                 except ValueError as e:
                     self.log.error(e)
-                    print mess, 139
+                    print 181, mess
                     self.log.error('TypeError: 140 lines')
-                    print self.url_request.post(self.url_dic['pollMessage'], data=data).text, 141
+                    print 183, self.url_request.post(self.url_dic['pollMessage'], data=data).text
 
-    def groupInfo(self, groupid):
+    def get_group_info(self, groupid):
         """
         According the GroupID, to set the GroupName_dic and GroupMem_dic
         """
-        self.log.info("Enter function groupInfo")
-
         def _hash_digest(uin, ptwebqq):
             """
             提取自http://pub.idqqimg.com/smartqq/js/mq.js
@@ -191,6 +210,7 @@ class SmartQQ():
                 V += N[U[T] & 15]
             return V
 
+        self.log.info("Enter function groupInfo")
         response = self.url_request.post(
             'http://s.web2.qq.com/api/get_group_name_list_mask2',
             {
@@ -206,13 +226,6 @@ class SmartQQ():
         if result['retcode'] == 0:
             for group in result['result']['gnamelist']:
                 self.groupName[str(group['gid'])] = group
-                #self.groupName[group['code']] = group
-                # if 'linux /shell/awk/sed' in group['name']:
-                #     group_info_list = [group['code'], group['gid'], group['name']]
-                #     targer_code = group['code']
-                #     targer_gid = group['gid']
-            # print groupid
-            # print self.groupName[groupid]['gid'], self.groupName[groupid]['code'],
             self.log.info('Get groupList success!')
             stamp = time.time() * 1000
             group_id = self.groupName[groupid]['code']     # qqqun code
@@ -220,8 +233,6 @@ class SmartQQ():
             url = self.url_dic['groupInfo'].format(group_id, self.vfwebqq, stamp)
             try:
                 member_list = json.loads(self.url_request.get(url).text)['result']['minfo']
-                # print json.loads(self.url_request.get(url).text)['result']
-                # print member_list
                 for member in member_list:
                     tmp_dic[str(member['uin'])] = member['nick']
                 self.groupMember[str(self.groupName[groupid]['gid'])] = tmp_dic
@@ -229,12 +240,11 @@ class SmartQQ():
                 print "KeyError The line is 187"
                 print json.loads(self.url_request.get(url).text)
 
-    def login(self):
-        self.getPara()
-        self.downQrcode()
-        self.checkLogin()
+    def main(self):
+        self.get_comm_para()
+        self.login()
 
 
 a = SmartQQ()
-a.login()
+a.main()
 a.poll()
