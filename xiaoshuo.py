a6dd6586ad5c1531400307a383a3598b5d02ca30
YINandXUAN <659030105@qq.com>
2016-08-23 22:34:23 +0800

    整理

--- xiaoshuo.py
+++
@@ -1,9 +0,0 @@
-# coding:ISO-8859-1
-import requests
-import pyquery
-
-url = 'http://www.szyangxiao.net/6554.shtml'
-h = requests.get(url)
-print h.content.decode('gbk').encode('utf8')
-print h.encoding
-
